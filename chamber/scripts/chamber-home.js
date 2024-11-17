fa7e633a4e56e148117406cc4ea71d0148d191a2
utahphotohunter <mitch.hunter1197@gmail.com>
2024-11-16 17:39:07 -0700

    working on json and js for chamber page

--- chamber/scripts/chamber-home.js
+++ chamber/scripts/chamber-home.js
@@ -15,3 +15,101 @@ listIcon.addEventListener("click", function() {
     viewDisplay.classList.remove("grid-view");
     viewDisplay.classList.add("list-view");
 });
+
+
+// business preview card population from json
+
+const url = "https://utahphotohunter.github.io/wdd231/chamber/data/members.json";
+const cards = document.getElementById("view-selector");
+
+async function logData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(businesses);
+    displayBusinesses(data.businesses);
+
+}
+
+logData();
+
+
+function displayBusinesses(data) {
+    data.forEach(business => {
+        // make card
+        const card = document.createElement("article");
+        card.setAttribute("id", business.id);
+        card.classList.add("simple-card");
+
+        // make card header
+        const cardHeader = document.createElement("div");
+        cardHeader.classList.add("container", "simple-header");
+        card.appendChild(cardHeader);
+
+        // make h3
+        const businessName = document.createElement("h3");
+        businessName.classList.add("secondary-text");
+        businessName.textContent = business.name;
+        cardHeader.appendChild(businessName);
+
+        // make tag line
+        const businessTagLine = document.createElement("p");
+        businessTagLine.classList.add("tag-line");
+        businessTagLine.textContent = business.tagLine;
+        cardHeader.appendChild(businessTagLine);
+
+        // make details button
+        const detailsButton = document.createElement("button");
+        detailsButton.classList.add("details", "btn");
+        detailsButton.textContent = "Details";
+        cardHeader.appendChild(detailsButton);
+
+        // make business info div
+        const businessInfo = document.createElement("div");
+        businessInfo.classList.add("container", "simple-info");
+
+        // make business image
+        const businessImage = document.createElement("img");
+        businessImage.classList.add("simple-image");
+        businessImage.setAttribute("src", business.img);
+        businessImage.setAttribute("alt", business.name);
+        businessImage.setAttribute("loading", "lazy");
+        businessInfo.appendChild(businessImage);
+
+        // make business info text
+        const businessText = document.createElement("div");
+        businessText.classList.add("container", "simple-text");
+        businessInfo.appendChild(businessText);
+
+        const infoList = document.createElement("ul");
+        businessText.appendChild(infoList);
+
+        // make email
+        const businessEmail = document.createElement("li");
+        const emailSpan = document.createElement("span");
+        emailSpan.classList.add("bold");
+        emailSpan.textContent = "Email: ";
+        businessEmail.appendChild(emailSpan);
+        businessEmail.append(business.email);
+        infoList.appendChild(businessEmail);
+
+        // make phone
+        const businessPhone = document.createElement("li");
+        const phoneSpan = document.createElement("span");
+        phoneSpan.classList.add("bold");
+        phoneSpan.textContent = "Phone: ";
+        businessPhone.appendChild(phoneSpan);
+        businessPhone.append(business.phoneNumber);
+        infoList.appendChild(businessPhone);
+
+        // make email
+        const businessUrl = document.createElement("li");
+        const urlSpan = document.createElement("span");
+        urlSpan.classList.add("bold");
+        urlSpan.textContent = "Url: ";
+        businessUrl.appendChild(urlSpan);
+        businessUrl.append(business.url);
+        infoList.appendChild(businessUrl);
+
+        cards.appendChild(card);
+    });
+}
