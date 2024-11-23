9ee8a8faafd72568bececacdd1fdb1be21942e9f
utahphotohunter <mitch.hunter1197@gmail.com>
2024-11-22 21:15:48 -0700

    finished home page for chamber site

--- chamber/scripts/chamber-home.js
+++ chamber/scripts/chamber-home.js
@@ -1,5 +1,5 @@
 // api url
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=f9e50fde67107e72150b984b2ef85827";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=40.28&lon=-111.68&units=imperial&appid=f9e50fde67107e72150b984b2ef85827";
 const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=49.75&lon=6.64&units=imperial&appid=f9e50fde67107e72150b984b2ef85827";
 const iconUrl = "https://api.openweathermap.org/img/w/";
 
@@ -29,9 +29,32 @@ function trasnlateDate(epoch) {
     return `${dayOfWeekName} ${months[monthNumber]}, ${dateNumber}`;
 }   
 
+function translateTime(epoch) {
+    let date = new Date(epoch * 1000);
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let meridian;
+
+    if (hour > 11) {
+        hour = hour - 12;
+        meridian = "pm";
+    } else if (hour == 0) {
+        hour = 12;
+        meridian = "am";
+    } else if (hour == 12) {
+        hour = 12;
+        meridian = "pm";
+    } else {
+        meridian = "am";
+    }
+
+    console.log(`${hour}:${minute}${meridian}`);
+    return `${hour}:${minute}${meridian}`;
+
+}
+
 function displayForecast(forecast) {
     let forecastNoonTimes = [forecast.list[3], forecast.list[11], forecast.list[19]];
-    // console.log(forecastNoonTimes[0]);
     const card = document.querySelector(".forecast");
 
     forecastNoonTimes.forEach(dataSet => {
@@ -67,7 +90,7 @@ function displayForecast(forecast) {
 
         const weatherHumidity = document.createElement("li");
         weatherHumidity.classList.add("forecast-humidity");
-        weatherHumidity.innerHTML = `<span class="bold">Humidity:</span> ${Math.round(dataSet.main.humidity)}`;
+        weatherHumidity.innerHTML = `<span class="bold">Humidity:</span> ${Math.round(dataSet.main.humidity)}%`;
         weatherList.appendChild(weatherHumidity);
 
         weatherInfo.appendChild(weatherList);
@@ -76,19 +99,57 @@ function displayForecast(forecast) {
     });
 }
 
-// function displayCurrent(current, data) {
+function displayCurrent(data) {
+    const currentWeatherContent = document.querySelector(".current-weather-content");
 
-// }
+    const currentWeatherDate = document.createElement("h3");
+    currentWeatherDate.classList.add("current-weather-date", "bold");
+    currentWeatherDate.textContent = trasnlateDate(data.dt);
+    currentWeatherContent.appendChild(currentWeatherDate);
 
+    const currentWeatherIcon = document.createElement("img");
+    currentWeatherIcon.classList.add("current-weather-icon");
+    currentWeatherIcon.setAttribute("src", `${iconUrl}${data.weather[0].icon}@2x.png`);
+    currentWeatherIcon.setAttribute("alt", data.weather[0].description);
+    currentWeatherContent.appendChild(currentWeatherIcon);
 
+    const currentWeatherInfo = document.createElement("ul");
+    currentWeatherContent.appendChild(currentWeatherInfo);
 
+    const currentWeatherTemp = document.createElement("li");
+    currentWeatherTemp.classList.add("current-weather-temp");
+    currentWeatherTemp.innerHTML = `<span class="bold">${Math.round(data.main.temp)}</span>&deg;F`;
+    currentWeatherInfo.appendChild(currentWeatherTemp);
 
+    const currentWeatherCondition = document.createElement("li");
+    currentWeatherCondition.classList.add("current-weather-conditions");
+    currentWeatherCondition.innerHTML = `<span class="bold">Conditions: </span>${data.weather[0].description}`;
+    currentWeatherInfo.appendChild(currentWeatherCondition);
+
+    const currentWeatherHigh = document.createElement("li");
+    currentWeatherHigh.classList.add("current-weather-high");
+    currentWeatherHigh.innerHTML = `<span class="bold">High: </span>${Math.round(data.main.temp_max)}&deg;F`;
+    currentWeatherInfo.appendChild(currentWeatherHigh);
+
+    const currentWeatherLow = document.createElement("li");
+    currentWeatherLow.classList.add("current-weather-low");
+    currentWeatherLow.innerHTML = `<span class="bold">Low: </span>${Math.round(data.main.temp_min)}&deg;F`;
+    currentWeatherInfo.appendChild(currentWeatherLow);
+
+    const currentWeatherHumidity = document.createElement("li");
+    currentWeatherHumidity.classList.add("current-weather-humidity");
+    currentWeatherHumidity.innerHTML = `<span class="bold">Humidity: </span>${Math.round(data.main.humidity)}%`;
+    currentWeatherInfo.appendChild(currentWeatherHumidity);
+}
 
 async function main() {
     let forecastData = await fetchApi(forecastUrl);
-    console.log(forecastData);
+    // console.log(forecastData);
+    let currentWeather = await fetchApi(weatherUrl);
+    // console.log(currentWeather);
 
     displayForecast(forecastData);
+    displayCurrent(currentWeather);
 }
 
 main();
