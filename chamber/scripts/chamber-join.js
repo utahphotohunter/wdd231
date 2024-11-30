84908b174c2d520275dfa2aefd96651b706d79c2
utahphotohunter <mitch.hunter1197@gmail.com>
2024-11-30 16:26:41 -0700

    finished styling the chamber join page for mobile

---
+++ chamber/scripts/chamber-join.js
@@ -0,0 +1,9 @@
+const dialog = document.querySelector(".business-modal");
+
+dialog.showModal();
+
+const modalCloseButton = document.querySelector(".modal-close");
+
+modalCloseButton.addEventListener("click", function () {
+    dialog.removeAttribute("open");
+})
